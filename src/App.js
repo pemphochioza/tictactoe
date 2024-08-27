b5759e06a04a38bc1a5b84f402cf5ef1edbc0146
Pempho Chioza <pemphochioza@gmail.com>
2024-08-27 13:39:50 +0200

    implemented the board and the Square component. Implemented State handling for the squares to record which square has been clicked

--- src/App.js
+++ src/App.js
@@ -1,3 +1,32 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}>
+    {value}
+    </button>;
+}
+
+export default function Board() {
+  const [squares, setSquare] = useState(Array(9).fill(null));
+
+  return (
+  <>
+    <div className="board-row">
+      <Square value={squares[0]} />
+      <Square value={squares[1]} />
+      <Square value={squares[2]} />
+    </div>
+    <div className="board-row">
+      <Square value={squares[3]} />
+      <Square value={squares[4]} />
+      <Square value={squares[5]} />
+    </div>
+    <div className="board-row">
+      <Square value={squares[6]} />
+      <Square value={squares[7]}/>
+      <Square value={squares[8]}/>
+    </div>
+    
+  </>
+  );
 }
